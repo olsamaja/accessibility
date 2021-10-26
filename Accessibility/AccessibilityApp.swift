860074517101ce0f7d7383d2b2b8d2c0ec8fd282
Olivier Rigault <olivier.rigault@newday.co.uk>
2021-10-26 15:44:05 +0100

    Initial Commit

---
+++ Accessibility/AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityApp.swift
+//  Accessibility
+//
+//  Created by Olivier Rigault on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by Olivier Rigault on 26/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
