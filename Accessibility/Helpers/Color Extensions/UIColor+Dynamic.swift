b01c728cb2758b0d4b7754e48446d57220c0a077
Olivier Rigault <olivier.rigault@newday.co.uk>
2021-10-29 14:59:29 +0100

    Add comments

--- Accessibility/Helpers/Color Extensions/UIColor+Dynamic.swift
+++ Accessibility/Helpers/Color Extensions/UIColor+Dynamic.swift
@@ -9,6 +9,8 @@ import UIKit
 
 extension UIColor {
     
+    /// Returns a dynamic colour used for foreground UI elements, such as labels.
+    ///
     var dynamic: UIColor {
         
         let dark = self.brighter()
@@ -18,6 +20,8 @@ extension UIColor {
         return dynamicColors(light: self, highContrast: highContrast, dark: dark, darkHighContrast: darkHighContrast)
     }
 
+    /// Returns a dynamic colour used for background UI elements, such as a section header background.
+    ///
     var dynamicBackground: UIColor {
         
         let dark = self.darker()
@@ -27,6 +31,16 @@ extension UIColor {
         return dynamicColors(light: self, highContrast: highContrast, dark: dark, darkHighContrast: darkHighContrast)
     }
     
+    /// Returns a dynamic colour based on user interface style and accessibility contrast setting.
+    ///
+    /// - parameter light: colour used for light mode and standard contrast
+    /// - parameter highContrast: colour used for light mode and increased (high) contrast
+    /// - parameter dark: colour used for dark mode and standard contrast
+    /// - parameter darkHighContrast: colour used for dark mode and increased (high) contrast
+    ///
+    /// - Unspecified user interface style is treated as light mode
+    /// - Unspecified accessibility contrast is treated as standard contrast
+    ///
     private func dynamicColors(light: UIColor, highContrast: UIColor, dark: UIColor, darkHighContrast: UIColor) -> UIColor {
         return UIColor(dynamicProvider: {
             switch ($0.userInterfaceStyle, $0.accessibilityContrast) {
